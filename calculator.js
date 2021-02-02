46030c99d32a7c49019c4d9a62ce038322668164
tylercchase <tylercchase@gmail.com>
2021-02-02 11:45:22 -0900

    Step 3-5: Comma and newline delimiter

--- calculator.js
+++ calculator.js
@@ -1,5 +1,16 @@
 function calculator(input) {
-  if(input.length == 0)
-    return 0;    
+  if(input.length == 0){
+    return 0;  
+  }
+  if(input.includes(',') || input.includes('\n')){
+    let output = 0;
+    const numbers = input.split(/,|\n/g);
+    for(number of numbers){
+      output += parseInt(number);
+    }
+    return output;
+  }else{
+    return parseInt(input);
+  }
 }
 module.exports = calculator;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,31 @@ const calculator = require('../calculator');
 test('Empty String returns 0', () => {
   expect(calculator('')).toBe(0);
 });
+test('Single number should return value', () => {
+  expect(calculator('1')).toBe(1);
+  expect(calculator('2')).toBe(2);
+  expect(calculator('100')).toBe(100);
+});
+test('Two numbers, comma delimited, returns the sum', () => {
+  expect(calculator('1,2')).toBe(3);
+  expect(calculator('2,2')).toBe(4);
+  expect(calculator('50,50')).toBe(100);
+});
+test('Two numbers, newline delimited, returns the sum', () => {
+  expect(calculator(`1
+  1`)).toBe(2);
+  expect(calculator(`2
+  2`)).toBe(4);
+  expect(calculator(`50
+  50`)).toBe(100);
+});
+test('Three numbers, delimited either way, returns the sum', () => {
+  expect(calculator(`1,1
+  1`)).toBe(3);
+  expect(calculator(`5
+  1
+  1`)).toBe(7);
+  expect(calculator(`33
+  33
+  33`)).toBe(99);
+});
